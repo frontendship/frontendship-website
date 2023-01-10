1087687040c11dced592b34ef6b701bee8d04553
Wralith <wralithdev@gmail.com>
2023-01-10 22:17:18 +0300

    refactor: update button component to use cva
    Add class-variance-authority to project

--- components/base/Button/Button.tsx
+++ components/base/Button/Button.tsx
@@ -1,41 +1,64 @@
-import { type TButtonSize, TButtonVariation } from './types';
+import { VariantProps, cva } from 'class-variance-authority';
 
-const buttonSizes: Record<TButtonSize, string> = {
-  sm: 'text-sm px-[14px] py-2',
-  md: 'text-sm px-4 py-[10px]',
-  lg: 'px-[18px] py-[10px]',
-  xl: 'px-5 py-3',
-  '2xl': 'text-lg px-7 py-4'
-};
-
-const buttonBase = 'font-semibold rounded-lg shadow-sm shadow-violet-100/5';
-
-const buttonStyles: Record<TButtonVariation, string> = {
-  default: `${buttonBase} text-white bg-violet-600 hover:bg-violet-700 focus:ring-4 ring-violet-100 border border-violet-600`,
-  outlined: `${buttonBase} text-black hover:border-gray-400 bg-white focus:ring-4 ring-gray-100 border border-gray-300`
-};
-
-const disabledButtonStyles: Record<TButtonVariation, string> = {
-  default: `${buttonBase} text-white bg-violet-200 hover:bg-violet-200 cursor-not-allowed border border-violet-200`,
-  outlined: `${buttonBase} text-black bg-gray-200 hover:bg-gray-200 border border-gray-300 cursor-not-allowed`
-};
+const buttonStyles = cva(
+  ['font-semibold', 'rounded-lg', 'border', 'shadow-sm', 'shadow-violet-100/5'],
+  {
+    variants: {
+      variant: {
+        filled: [
+          'text-white',
+          'bg-violet-600',
+          'border-violet-600',
+          'hover:bg-violet-700',
+          'focus:ring-4',
+          'ring-violet-100',
+          'disabled:bg-violet-200',
+          'disabled:border-violet-200',
+          'disabled:cursor-not-allowed'
+        ],
+        outline: [
+          'text-black',
+          'bg-white',
+          'border-gray-300',
+          'hover:border-gray-400',
+          'focus:ring-4',
+          'ring-gray-100',
+          'disabled:bg-gray-200',
+          'disabled:cursor-not-allowed'
+        ]
+      },
+      fullWidth: {
+        true: 'w-full'
+      },
+      size: {
+        sm: ['text-sm', 'px-[14px]', 'py-2'],
+        md: ['text-sm', 'px-4', 'py-[10px]'],
+        lg: ['px-[18px]', 'py-[10px]'],
+        xl: ['px-5', 'py-3'],
+        '2xl': ['text-lg', 'px-7', 'py-4']
+      }
+    },
+    defaultVariants: {
+      variant: 'filled',
+      size: 'md'
+    }
+  }
+);
 
 // TODO: Icon
-interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  size?: TButtonSize;
-  variation?: TButtonVariation;
-}
+type ButtonBaseProps = VariantProps<typeof buttonStyles>;
+interface Props
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    ButtonBaseProps {}
 
-const Button = ({ className, ...props }: Props) => {
-  const size = buttonSizes[props.size ?? 'md'];
-  const variation = props.variation ?? 'default';
-  let style = props.disabled
-    ? disabledButtonStyles[variation]
-    : buttonStyles[variation];
-
-  const classes = [size, style, className].join(' ').trim();
-
-  return <button {...props} className={classes} />;
-};
+/**
+ * @example <Button variant="filled" size="lg" fullWidth>Click!</Button>
+ *  */
+const Button = ({ className, variant, fullWidth, size, ...props }: Props) => (
+  <button
+    {...props}
+    className={buttonStyles({ className, variant, size, fullWidth })}
+  />
+);
 
 export default Button;
--- components/base/Button/types.ts
+++
@@ -1,2 +0,0 @@
-export type TButtonSize = 'sm' | 'md' | 'lg' | 'xl' | '2xl';
-export type TButtonVariation = 'default' | 'outlined';

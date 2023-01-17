1acdb4f942a3b18753ed1c51a3ee432fd1b78566
osmandlsmn <dlsmnosman@gmail.com>
2023-01-17 16:54:30 +0300

    refactor(input.tsx): remove variant, add errorMessage

--- components/Input/Input.classes.ts
+++ components/Input/Input.classes.ts
@@ -1,24 +1,17 @@
 import { cva } from 'class-variance-authority';
 
-export default cva(['py-2.5 px-3.5 border transition-all'], {
-  variants: {
-    variant: {
-      default: [
-        'text-gray-800 shadow-sm rounded-lg border-gray-300 placeholder:text-gray-500',
-        'focus:outline-none focus:ring-2 focus:ring-violet-300 focus:border-violet-300',
-        'disabled:opacity-50 disabled:cursor-not-allowed'
-      ]
-    },
-    error: {
-      true: '!border-red-300 focus:ring-2 focus:ring-red-100'
-    },
-    size: {
-      sm: 'text-sm',
-      md: 'text-base'
+export default cva(
+  [
+    'py-2.5 px-3.5 border transition-all',
+    'text-gray-800 shadow-sm rounded-lg border-gray-300 placeholder:text-gray-500',
+    'focus:outline-none focus:ring-2 focus:ring-violet-300 focus:border-violet-300',
+    'disabled:opacity-50 disabled:cursor-not-allowed'
+  ],
+  {
+    variants: {
+      error: {
+        true: '!border-red-300 focus:ring-2 focus:ring-red-100'
+      }
     }
-  },
-  defaultVariants: {
-    variant: 'default',
-    size: 'md'
   }
-});
+);
--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,57 +1,30 @@
-import { forwardRef } from 'react';
-import { VariantProps } from 'class-variance-authority';
-import { FieldError, useFormContext } from 'react-hook-form';
 import InputClasses from './Input.classes';
 
-interface InputBaseProps extends VariantProps<typeof InputClasses> {
+// TODO: Icon will be displayed in error situations
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
-  name: string;
+  errorMessage?: string;
 }
 
-// TODO: Icon will be displayed in error situations
-type Props = React.InputHTMLAttributes<HTMLInputElement> & InputBaseProps;
+const ErrorField = ({ errorMessage }: { errorMessage?: string }) => {
+  return <div className="mt-1.5 text-red-500">{errorMessage}</div>;
+};
 
-/**
- * @example <Input name="email" label="Email" placeholder="olivia@untitledui.com"  />
- *  */
-const Input = forwardRef<HTMLInputElement, Props>(
-  ({ className, variant, label, size, ...rest }, ref) => {
-    const {
-      formState: { errors }
-    } = useFormContext();
-
-    return (
-      <div>
-        {label && (
-          <label className="block font-medium text-sm mb-1 text-gray-800">
-            {label}
-          </label>
-        )}
-        <input
-          className={InputClasses({
-            variant,
-            size,
-            error: !!errors[rest.name]
-          })}
-          ref={ref}
-          {...rest}
-        />
-        <ErrorField name={rest.name} />
-      </div>
-    );
-  }
-);
-
-const ErrorField = ({ name }: { name: string }) => {
-  const {
-    formState: { errors }
-  } = useFormContext();
-
-  const error = errors[name] as FieldError;
-
+const Input: React.FC<Props> = ({ errorMessage, label, ...props }) => {
   return (
-    <div className="text-red-500 shadow-xs shadow-red-300 mt-1.5">
-      {error?.message}
+    <div className="inline-block">
+      {label && (
+        <label className="mb-1 block text-sm font-medium text-gray-700">
+          {label}
+        </label>
+      )}
+      <input
+        className={InputClasses({
+          error: Boolean(errorMessage)
+        })}
+        {...props}
+      />
+      {errorMessage && <ErrorField errorMessage={errorMessage} />}
     </div>
   );
 };

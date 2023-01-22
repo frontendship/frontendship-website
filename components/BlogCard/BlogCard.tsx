5a772d6583f9c6cf0238bcd899810d96c8469e2e
seymenklc <seymenklc@gmail.com>
2023-01-22 14:30:28 +0300

    chore: change BlogCard prop name

--- components/BlogCard/BlogCard.tsx
+++ components/BlogCard/BlogCard.tsx
@@ -1,5 +1,5 @@
 import Image, { ImageProps } from 'next/image';
-import Link, { LinkProps } from 'next/link';
+import Link from 'next/link';
 import { ReactNode } from 'react';
 import { ArrowUpRight } from 'react-feather';
 
@@ -15,29 +15,23 @@ const Description = ({ children }: Props) => {
   return <p className="text-gray-500">{children}</p>;
 };
 
-const Img = ({ attr }: { attr: ImageProps; }) => {
+const Img = ({ attr }: { attr: ImageProps }) => {
   return <Image {...attr} />;
 };
 
-const Info = ({ by, date }: { by: string; date: string; }) => {
+const Info = ({ author, date }: { author: string; date: string }) => {
   return (
     <div className="text-sm font-semibold text-indigo-700">
-      <span>{by}</span>
+      <span>{author}</span>
       <span className="mx-1">•</span>
       <span>{date}</span>
     </div>
   );
 };
 
-const Title = ({
-  children,
-  link
-}: {
-  children: ReactNode;
-  link: LinkProps;
-}) => {
+const Title = ({ children, href }: { children: ReactNode; href: string }) => {
   return (
-    <Link {...link} className='flex items-center justify-between'>
+    <Link href={href} className="flex items-center justify-between">
       <h3 className="text-2xl font-semibold text-gray-900">{children}</h3>
       <ArrowUpRight />
     </Link>

8b97cb5a23e1d25135c390e4ce7007803a7eeaf1
oguzergul <oguz.ergul@trendyol.com>
2023-03-10 06:47:59 +0300

    chore: export icon components from icons file for named imports

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,19 @@
+export { default as AngularIcon } from './angular.svg';
+export { default as DockerIcon } from './docker.svg';
+export { default as FigmaIcon } from './figma.svg';
+export { default as GitIcon } from './git.svg';
+export { default as GithubIcon } from './github.svg';
+export { default as HTMLIcon } from './html.svg';
+export { default as JavaScriptIcon } from './js.svg';
+export { default as NodeIcon } from './node.svg';
+export { default as NpmIcon } from './npm.svg';
+export { default as ReactIcon } from './react.svg';
+export { default as ReduxIcon } from './redux.svg';
+export { default as VisualStudioIcon } from './visual.svg';
+export { default as VueIcon } from './vue.svg';
+export { default as WebpackIcon } from './webpack.svg';
+export { default as TypeScriptIcon } from './ts.svg';
+
+export { default as TwitterIcon } from './twitter.svg';
+export { default as FacebookIcon } from './facebook.svg';
+export { default as LinkedinIcon } from './linkedin.svg';
